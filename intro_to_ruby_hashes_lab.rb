2247c69c8a651d4310a9358b26787eb14b5cc236
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-11 20:03:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,8 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {:first_name => "Jacob", :last_name => "Kenny"}
+  my_hash
 end
 
 def pioneer
